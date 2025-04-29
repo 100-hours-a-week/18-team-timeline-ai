402c18c27d5a1c1fc38b72aee3555f3e7f029edc
Lockway <lchmax37@gmail.com>
2025-04-29 13:21:55 +0900

    main과 router 작성

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-from summarization import summarize
+from fastapi import FastAPI
+from api import router as api_router
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI(title="AI News Timeline API", version="1.0.0")
+
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow_origins=["https://backend.com"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# API 라우터 등록
+app.include_router(api_router, prefix="/api")
+
+# 테스트용 엔드포인트
+@app.get("/ping")
+def ping():
+    return {"status": "ok"}
+
+
+'''
+from summarization import summarize
 URL = "https://www.yna.co.kr/view/AKR20250414002900071?site=popularnews_view"
 ret = summarize(URL=URL)
 print(ret)
+'''
