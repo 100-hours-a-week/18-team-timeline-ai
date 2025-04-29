265ad628fd36951fecac8081b554ee34ed939b0f
Lockway <lchmax37@gmail.com>
2025-04-29 13:31:05 +0900

    main.py에서 CORS 삭제

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 from fastapi import FastAPI
 from api import router as api_router
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="AI News Timeline API", version="1.0.0")
 
-# CORS 설정
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # allow_origins=["https://backend.com"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 # API 라우터 등록
 app.include_router(api_router, prefix="/api")
@@ -19,7 +11,6 @@ app.include_router(api_router, prefix="/api")
 @app.get("/ping")
 def ping():
     return {"status": "ok"}
-
 
 '''
 from summarization import summarize
