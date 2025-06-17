f8c821178b4c29aa03a21368baca0a4e9254ece8
Lockway <lchmax37@gmail.com>
2025-06-17 15:22:03 +0900

    뉴스 없는 경우 예외처리

--- scrapers/serper.py
+++ scrapers/serper.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import date, timedelta
-from util.timeline_utils import available_url, auto_clean_url
+from utils.error_utils import error_response
+from utils.timeline_utils import available_url, auto_clean_url
 
 # ---------------------------------------------------
 
@@ -78,6 +79,8 @@ def distribute_news_serper(
         max_count = -1
         best_news = None
         news_list = get_news_serper(query, current, api_key)
+        if not news_list:
+            return error_response(404, f"{current}에 해당하는 기사가 없습니다!")
 
         # 검색어가 많이 나타난 뉴스 찾기
         for link, title in news_list:
