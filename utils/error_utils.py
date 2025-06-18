987bf0c442623c11aabee7d4d7d5ca281d008eba
Lockway <lchmax37@gmail.com>
2025-06-18 11:09:38 +0900

    error_response 다시 동기화

--- utils/error_utils.py
+++ utils/error_utils.py
@@ -1,10 +1,9 @@
 from fastapi.responses import JSONResponse
 from schemas.response_schema import ErrorResponse
-import asyncio
 
 
-async def error_response(code, msg: str):
-    await asyncio.sleep(0.1)  # 100ms 지연
+def error_response(code, msg: str):
     return JSONResponse(
-        status_code=code, content=ErrorResponse(success=False, message=msg).model_dump()
+        status_code=code,
+        content=ErrorResponse(success=False, message=msg).model_dump()
     )
